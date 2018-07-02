729033b19f908dfcbd7013b5907c8b7827bb387d
Eric Jinks <ericjinks@gmail.com>
2018-07-02 16:34:28 +1000

    JoinBanner css

--- src/components/JoinBanner.js
+++ src/components/JoinBanner.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import Button from './Button'
+import Wave from './Wave'
 import './JoinBanner.css'
 
 export default ({ linkTo = '/', ...props }) => (
   <div className="col5 JoinBanner">
     <h3 className="JoinBanner--title">Join Our Community</h3>
+    <Wave />
     <Button to={linkTo}>Enrol Now</Button>
   </div>
 )

8042f82ba25d05a7848ea0adaf91a111027b2fe1
Eric Jinks <ericjinks@gmail.com>
2018-07-09 13:43:09 +1000

    netlify-cms update

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import CMS from 'netlify-cms'
 
+// import { HomePageTemplate } from '../templates/HomePage'
+
 if (process.env.NETLIFY_SITE_URL && typeof window !== 'undefined') {
   window.localStorage.setItem('netlifySiteURL', process.env.NETLIFY_SITE_URL)
 }
 
-import { HomePageTemplate } from '../templates/HomePage'
-
-CMS.registerPreviewStyle('/styles.css')
-CMS.registerPreviewTemplate('home-page', ({ entry }) => <HomePageTemplate />)
+// CMS.registerPreviewTemplate('home-page', ({ entry }) => <HomePageTemplate />)

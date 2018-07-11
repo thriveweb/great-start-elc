f9978f86b90d72db4627f0c841787a45567394cd
Eric Jinks <ericjinks@gmail.com>
2018-07-11 16:28:34 +1000

    Contact <Layout />

--- src/templates/ContactPage.js
+++ src/templates/ContactPage.js
@@ -3,6 +3,7 @@ import React from 'react'
 import PageHeader from '../components/PageHeader'
 import EnquiryFormSimpleAjax from '../components/EnquiryFormSimpleAjax'
 import Content from '../components/Content'
+import Layout from '../components/Layout'
 import { MapPin, Smartphone, Mail } from 'react-feather'
 import './ContactPage.css'
 
@@ -16,50 +17,52 @@ export const ContactPageTemplate = ({
   phone,
   email
 }) => (
-  <main className="Contact">
-    <PageHeader
-      title={title}
-      subtitle={subtitle}
-      backgroundImage={featuredImage}
-    />
+  <Layout>
+    <main className="Contact">
+      <PageHeader
+        title={title}
+        subtitle={subtitle}
+        backgroundImage={featuredImage}
+      />
 
-    <div className="section Contact--Section1">
-      <div className="container Contact--Section1--Container">
-        <div>
-          <Content source={body} />
+      <div className="section Contact--Section1">
+        <div className="container Contact--Section1--Container">
+          <div>
+            <Content source={body} />
 
-          <div className="Contact--Details">
-            {address && (
-              <a
-                className="Contact--Details--Item"
-                href={`https://www.google.com.au/maps/search/${encodeURI(
-                  address
-                )}`}
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                <MapPin /> {address}
-              </a>
-            )}
-            {phone && (
-              <a className="Contact--Details--Item" href={`tel:${phone}`}>
-                <Smartphone /> {phone}
-              </a>
-            )}
-            {email && (
-              <a className="Contact--Details--Item" href={`mailto:${email}`}>
-                <Mail /> {email}
-              </a>
-            )}
+            <div className="Contact--Details">
+              {address && (
+                <a
+                  className="Contact--Details--Item"
+                  href={`https://www.google.com.au/maps/search/${encodeURI(
+                    address
+                  )}`}
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <MapPin /> {address}
+                </a>
+              )}
+              {phone && (
+                <a className="Contact--Details--Item" href={`tel:${phone}`}>
+                  <Smartphone /> {phone}
+                </a>
+              )}
+              {email && (
+                <a className="Contact--Details--Item" href={`mailto:${email}`}>
+                  <Mail /> {email}
+                </a>
+              )}
+            </div>
+          </div>
+
+          <div>
+            <EnquiryFormSimpleAjax name="Simple Form Ajax" />
           </div>
         </div>
-
-        <div>
-          <EnquiryFormSimpleAjax name="Simple Form Ajax" />
-        </div>
       </div>
-    </div>
-  </main>
+    </main>
+  </Layout>
 )
 
 const ContactPage = ({ data }) => {

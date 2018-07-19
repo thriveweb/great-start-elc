9162de6fc1bc2cb99cbe5c0600cc0a07b4f19a31
Eric Jinks <ericjinks@gmail.com>
2018-07-19 10:48:30 +1000

    Update postcss config

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,16 +55,16 @@ module.exports = {
       }
     },
     // css
-    // {
-    //   resolve: `gatsby-plugin-postcss-sass`,
-    //   options: {
-    //     postCssPlugins: [
-    //       postcssPresetEnv({
-    //         browsers: '> 0.5%, last 2 versions, ie 11'
-    //       })
-    //     ]
-    //   }
-    // },
+    {
+      resolve: `gatsby-plugin-postcss-sass`,
+      options: {
+        postCssPlugins: [
+          postcssPresetEnv({
+            browsers: '> 0.5%, last 2 versions, ie 11'
+          })
+        ]
+      }
+    },
     'gatsby-plugin-sitemap',
 
     {

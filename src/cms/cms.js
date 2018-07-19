d7e9c6c0777fb08e3f8620a92c4206adc65223ae
Eric Jinks <ericjinks@gmail.com>
2018-07-19 11:18:48 +1000

    Add HomePage to CMS

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,8 +3,23 @@ import CMS from 'netlify-cms'
 
 import { HomePageTemplate } from '../templates/HomePage'
 
+CMS.registerPreviewStyle('/styles.css')
+
+CMS.registerPreviewTemplate('home-page', ({ entry }) => (
+  <HomePageTemplate {...entry.toJS().data} />
+))
+
 if (process.env.NETLIFY_SITE_URL && typeof window !== 'undefined') {
   window.localStorage.setItem('netlifySiteURL', process.env.NETLIFY_SITE_URL)
 }
 
-CMS.registerPreviewTemplate('home-page', ({ entry }) => <HomePageTemplate />)
+// Log netlifySiteURL if editing on localhost
+if (
+  window.location.hostname === 'localhost' &&
+  window.localStorage.getItem('netlifySiteURL')
+) {
+  console.log(
+    `%cnetlifySiteURL: ${window.localStorage.getItem('netlifySiteURL')}`,
+    'color: hotpink; font-size: 15px'
+  )
+}

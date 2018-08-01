98b1ff759cfcfbebfdc649068dd352db4a756052
Amber Gowans <amber@thriveweb.com.au>
2018-08-01 12:30:39 +1000

    fix banner background-image

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -7,27 +7,11 @@ import headerMain from '../images/header-main.png'
 import header from '../images/header.png'
 import './PageHeader.css'
 
-const PageHeader = ({
-  title,
-  subtitle,
-  backgroundImage,
-  large,
-  button,
-  className = ''
-}) => {
+const PageHeader = ({ title, subtitle, backgroundImage, large, button, className = '' }) => {
+
   if (large) className += ' PageHeader-large'
   return (
     <div className={`PageHeader relative ${className}`}>
-      <Image
-        background
-        src={large ? header : headerMain}
-        className="PageHeader--BG"
-        backgroundSize="auto 100%"
-      />
-      {large &&
-        backgroundImage && (
-          <Image background src={backgroundImage} opacity={0.4} />
-        )}
       <div className="container relative content">
         <div className="PageHeader--Inner">
           <h1 className="PageHeader--Title">{title}</h1>

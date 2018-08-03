83c423cc368789eba3160b55dcf2b81cb215b1ce
Amber Gowans <amber@thriveweb.com.au>
2018-08-03 15:59:06 +1000

    maybe fixed?

---
+++ src/templates/DownloadPages.js
@@ -0,0 +1,59 @@
+import React, { Fragment } from 'react'
+import Helmet from 'react-helmet'
+
+import PageHeader from '../components/PageHeader'
+import Content from '../components/Content'
+import './DefaultPage.css'
+
+// Export Template for use in CMS preview
+export const DownloadPagesTemplate = ({
+  title,
+  subtitle,
+  downloadBanner,
+  body
+}) => {
+
+  return (
+    <main className="DownloadPage background-dots">
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+
+      <PageHeader title={title} subtitle={subtitle} />
+
+      <section className="section">
+        <div className="container content">
+          <Content source={body} />
+        </div>      
+      </section>
+    </main>
+  )
+}
+
+const DownloadPages = ({ data: { page } }) => (
+  <DownloadPagesTemplate {...page} {...page.frontmatter} body={page.html} />
+)
+
+export default DownloadPages
+
+export const pageQuery = graphql`
+  query DownloadPages($id: String!) {
+    page: markdownRemark(id: { eq: $id }) {
+      html
+      rawMarkdownBody
+      frontmatter {
+        title
+        subtitle
+        downloadBanner {
+          file {
+            publicURL
+          }
+          title
+          preview {
+            ...SmallImage
+          }
+        }
+      }
+    }
+  }
+`

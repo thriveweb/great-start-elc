2f8051115fc3cb3a12639e79b74139972cd48595
Amber Gowans <amber@thriveweb.com.au>
2018-08-08 20:51:06 +1000

    test

--- src/components/DownloadBox.js
+++ src/components/DownloadBox.js
@@ -6,15 +6,15 @@ import './DownloadBox.css'
 export default ({ listItems = [] }) => (
   <div className="DownloadBox hasBorder hasShadow">
     <h5 className="DownloadBox--Title">Downloadable Forms</h5>
-    {listItems.map(item => (
-      <a
+    {listItems.map(item => {
+     return <a
         key={item.title}
         target="_blank"
-        href={item.file}
+        href={item.file.publicURL}
         className="DownloadBox--Item"
       >
         <File /> {item.title}
       </a>
-    ))}
+    })}
   </div>
 )
--- src/components/ExceedBanner.js
+++ src/components/ExceedBanner.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import _get from 'lodash/get'
 import PopoutBanner from './PopoutBanner'
 import './ExceedBanner.css'
 
 export default ({ footerSettings }) => {
   const { exceedLogo, exceedText, exceedTextLong, long } = footerSettings
+
+  if(!!footerSettings.length) return null
 
   return long ? (
     <PopoutBanner image={exceedLogo} title={exceedTextLong} />

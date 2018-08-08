e3f4b7d1b1bba4b9caf791f373422d2ba34604ae
Amber Gowans <amber@thriveweb.com.au>
2018-08-08 14:49:23 +1000

    test

--- src/templates/DefaultPage.js
+++ src/templates/DefaultPage.js
@@ -21,13 +21,13 @@ export const DefaultPageTemplate = ({
   popoutBanner,
   accordion,
   downloadableForms,
-  // infoSection,
+  infoSection,
   footerSettings,
   body
 }) => {
 
-  // const description = _get(infoSection, 'description') || ''
-  // const infoListing = _get(infoSection, 'infoListing') || []
+  const description = _get(infoSection, 'description') || ''
+  const infoListing = _get(infoSection, 'infoListing') || []
 
   return (
     <main className="DefaultPage background-dots">
@@ -49,10 +49,10 @@ export const DefaultPageTemplate = ({
             <Accordion items={accordion} />
           </div>
         )}
-        {/*<InfoListing description={description} infoListing={infoListing} />*/}        
+        <InfoListing description={description} infoListing={infoListing} />        
       </section>
 
-      {popoutBanner && (d
+      {popoutBanner && (
         <PopoutBanner image={popoutBanner.image} title={popoutBanner.title} />
       )}
       <div className="section thin JoinBannerSection">
@@ -104,6 +104,16 @@ export const pageQuery = graphql`
         accordion {
           title
           content
+        }
+        infoSection {
+          description
+          infoListing {
+            icon {
+              ...SmallImage
+            }
+            title
+            content
+          }
         }
       }
     }

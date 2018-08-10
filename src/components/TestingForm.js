e103d059c69af4288cc81e668b5783c37954aeed
Amber Gowans <amber@thriveweb.com.au>
2018-08-10 11:01:35 +1000

    form test

--- src/components/TestingForm.js
+++ src/components/TestingForm.js
@@ -7,7 +7,7 @@ import './EnquiryForm.css'
 
 class Form extends React.Component {
   static defaultProps = {
-    name: 'Testing Form',
+    name: 'New',
     subject: '', // optional subject of the notification email
     action: '',
     successMessage: 'Thanks for your enquiry, we will get back to you soon',
@@ -70,36 +70,57 @@ class Form extends React.Component {
     const { name, subject, action } = this.props
 
     return (
-      <form
-        className="ApplicationForm"
-        name={name}
-        action={action}
-        onSubmit={this.handleSubmit}
-        data-netlify=""
-        data-netlify-honeypot="email"
-      >
-        {this.state.alert && (
-          <div className="EnquiryForm--Alert">{this.state.alert}</div>
-        )}
-        <label className="EnquiryForm--Label">
-          <input
-            className="EnquiryForm--Input"
-            type="text"
-            placeholder="Your Name"
-            name="name"
-            required
-          />
-        </label>
-        <input type="text" name="_gotcha" style={{ display: 'none' }} />
-        {!!subject && <input type="hidden" name="subject" value={subject} />}
-        <input type="hidden" name="form-name" value={name} />
-          <input
-            className="Button hasShadowHover EnquiryForm--SubmitButton"
-            type="submit"
-            value="Testing"
-            disabled={this.state.disabled}
-          />
-      </form>
+      <section className='download-banner'>
+        <Image src='/images/uploads/handbook.jpg' alt='image of handbook' />
+        <form
+          className="DownloadForm"
+          name={name}
+          action={action}
+          onSubmit={this.handleSubmit}
+          data-netlify=""
+          data-netlify-honeypot="email"
+        >
+          {this.state.alert && (
+            <div className="EnquiryForm--Alert">{this.state.alert}</div>
+          )}
+          <h3 className='form-description'>Download our Family Handbook for all you need to know about enroling your child at one of our centres.</h3>
+          <label className="EnquiryForm--Label">
+            <input
+              className="EnquiryForm--Input"
+              type="text"
+              placeholder="Your Name"
+              name="name"
+              required
+            />
+          </label>
+          <label className="EnquiryForm--Label">
+            <input
+              className="EnquiryForm--Input"
+              type="email"
+              placeholder="Email"
+              name="e-mail"
+              required
+            />
+          </label>
+          <div className='EnquiryForm--Label label-text'>
+            <p>Choose Centre:</p>
+            {[ 
+              this.renderOption('centre', 'East Malvern'),
+              this.renderOption('centre', 'Mildura'),
+              this.renderOption('centre', 'Mildura Central')
+            ]}
+          </div>
+          <input type="text" name="_gotcha" style={{ display: 'none' }} />
+          {!!subject && <input type="hidden" name="subject" value={subject} />}
+          <input type="hidden" name="form-name" value={name} />
+            <input
+              className="Button hasShadowHover EnquiryForm--SubmitButton"
+              type="submit"
+              value="Download"
+              disabled={this.state.disabled}
+            />
+        </form>
+      </section>  
     )
   }
 }

4bd5ebf1598ca15bae2fc8e0e65bdd6003b99276
Amber Gowans <amber@thriveweb.com.au>
2019-05-14 07:56:46 -0700

    cms error

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -10,26 +10,26 @@ import { CentreTemplate } from '../templates/Centre'
 import { HealthInfoPageTemplate } from '../templates/HealthInfoPage'
 import { FamilyChecklistTemplate } from '../templates/FamilyChecklist'
 
-CMS.registerPreviewStyle('/styles.css')
-
-CMS.registerPreviewTemplate('home-page', ({ entry }) => (
-  <HomePageTemplate {...entry.toJS().data} />
-))
-CMS.registerPreviewTemplate('defaultPages', ({ entry }) => (
-  <DefaultPageTemplate {...entry.toJS().data} />
-))
-CMS.registerPreviewTemplate('defaultPages', ({ entry }) => (
-  <DownloadPagesTemplate {...entry.toJS().data} />
-))
-CMS.registerPreviewTemplate('team-members', ({ entry }) => (
-  <TeamMembersTemplate {...entry.toJS().data} />
-))
-CMS.registerPreviewTemplate('centres', ({ entry }) => (
-  <CentreTemplate {...entry.toJS().data} />
-))
-CMS.registerPreviewTemplate('health-info-page', ({ entry }) => (
-  <HealthInfoPageTemplate {...entry.toJS().data} />
-))
-CMS.registerPreviewTemplate('family-checklist', ({ entry }) => (
-  <FamilyChecklistTemplate {...entry.toJS().data} />
-))
+// CMS.registerPreviewStyle('/styles.css')
+//
+// CMS.registerPreviewTemplate('home-page', ({ entry }) => (
+//   <HomePageTemplate {...entry.toJS().data} />
+// ))
+// CMS.registerPreviewTemplate('defaultPages', ({ entry }) => (
+//   <DefaultPageTemplate {...entry.toJS().data} />
+// ))
+// CMS.registerPreviewTemplate('defaultPages', ({ entry }) => (
+//   <DownloadPagesTemplate {...entry.toJS().data} />
+// ))
+// CMS.registerPreviewTemplate('team-members', ({ entry }) => (
+//   <TeamMembersTemplate {...entry.toJS().data} />
+// ))
+// CMS.registerPreviewTemplate('centres', ({ entry }) => (
+//   <CentreTemplate {...entry.toJS().data} />
+// ))
+// CMS.registerPreviewTemplate('health-info-page', ({ entry }) => (
+//   <HealthInfoPageTemplate {...entry.toJS().data} />
+// ))
+// CMS.registerPreviewTemplate('family-checklist', ({ entry }) => (
+//   <FamilyChecklistTemplate {...entry.toJS().data} />
+// ))
